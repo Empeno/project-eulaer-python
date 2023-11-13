fc3519782013d69b20e9ead26d162af01d447ec6
Morten Empeño <morten.empeno.ext@siemensgamesa.com>
2023-11-13 13:32:48 +0100

    .

--- tests/test_problem_one.py
+++ tests/test_problem_one.py
@@ -1,31 +1,28 @@
-""" test_problem_one.py """
+import pytest
 
 from problem_one import solve_problem_one
 from services.math_service import MathService
-import pytest
 
-""" Fixture to create an instance of MathService """
+
+
 @pytest.fixture
 def math_service_instance():
     return MathService()
 
-""" Fixture to create a mock instance of MathService for testing """
+
 @pytest.fixture
 def math_service_mock():
     class MathServiceMock:
         def sum_of_multiples(self, limit):
-            # Mocked implementation for testing
             return 42
 
     return MathServiceMock()
 
-""" Test the solve_problem_one function with a real MathService instance """
 def test_solve_problem_one_with_real_math_service(math_service_instance, capsys):
     solve_problem_one(math_service_instance)
     captured = capsys.readouterr()
     assert "Sum of multiples of 3 or 5 below 1000:" in captured.out
 
-""" Test the solve_problem_one function with a mocked MathService instance """
 def test_solve_problem_one_with_mocked_math_service(math_service_mock, capsys):
     solve_problem_one(math_service_mock)
     captured = capsys.readouterr()
