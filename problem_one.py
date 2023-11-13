2582bcda81936b5bd5416d902a39b23c6a3ff99b
Morten Empeño <morten.empeno.ext@siemensgamesa.com>
2023-11-13 10:33:36 +0100

    docstring

--- problem_one.py
+++ problem_one.py
@@ -1,7 +1,14 @@
+"""
+Problem One module: Solves a specific problem using MathService.
+
+Example:
+```python
 from math_service import MathService
 
-math_service_instance = MathService()
+result = MathService().sum_of_multiples(1000)
+print("Sum of multiples of 3 or 5 below 1000:", result)
+"""
+from math_service import MathService
 
-limit = 1000
-result = math_service_instance.sum_of_multiples(limit)
-print("The sum of multiples of 3 or 5 below 1000 is:", result)
+result = MathService().sum_of_multiples(1000)
+print("Sum of multiples of 3 or 5 below 1000:", result)
