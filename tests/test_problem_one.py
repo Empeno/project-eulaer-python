be87f94884ddde17c9fb9bcfa256577662a59aa8
Morten Empeño <morten.empeno.ext@siemensgamesa.com>
2023-11-13 13:31:37 +0100

    docstring

--- tests/test_problem_one.py
+++ tests/test_problem_one.py
@@ -1,15 +1,15 @@
-# test_problem_one.py
+""" test_problem_one.py """
 
 from problem_one import solve_problem_one
 from services.math_service import MathService
 import pytest
 
-# Fixture to create an instance of MathService
+""" Fixture to create an instance of MathService """
 @pytest.fixture
 def math_service_instance():
     return MathService()
 
-# Fixture to create a mock instance of MathService for testing
+""" Fixture to create a mock instance of MathService for testing """
 @pytest.fixture
 def math_service_mock():
     class MathServiceMock:
@@ -19,13 +19,13 @@ def math_service_mock():
 
     return MathServiceMock()
 
-# Test the solve_problem_one function with a real MathService instance
+""" Test the solve_problem_one function with a real MathService instance """
 def test_solve_problem_one_with_real_math_service(math_service_instance, capsys):
     solve_problem_one(math_service_instance)
     captured = capsys.readouterr()
     assert "Sum of multiples of 3 or 5 below 1000:" in captured.out
 
-# Test the solve_problem_one function with a mocked MathService instance
+""" Test the solve_problem_one function with a mocked MathService instance """
 def test_solve_problem_one_with_mocked_math_service(math_service_mock, capsys):
     solve_problem_one(math_service_mock)
     captured = capsys.readouterr()
