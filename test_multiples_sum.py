bb49b954f882d466a3bbfbe916e31433745e9845
Morten Empeño <morten.empeno.ext@siemensgamesa.com>
2023-11-13 10:34:58 +0100

    docstring

--- test_multiples_sum.py
+++ test_multiples_sum.py
@@ -1,7 +1,17 @@
-# test_math_service.py
+"""
+Test module for MathService: Tests the sum_of_multiples method.
+"""
+
 from math_service import MathService
 
 def test_sum_of_multiples():
+    """
+    Test the sum_of_multiples method of MathService.
+
+    Test cases:
+    1. limit is 10, expected result is 23.
+    2. limit is 1000, expected result is 233168.
+    """
     math_service_instance = MathService()
 
     # Test case 1: limit is 10
