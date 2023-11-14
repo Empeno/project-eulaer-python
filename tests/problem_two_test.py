db3b701ed409cb06494c399651b6e498bcf4b6bc
Morten Empeño <morten.empeno.ext@siemensgamesa.com>
2023-11-14 10:44:38 +0100

    .

--- tests/problem_two_test.py
+++ tests/problem_two_test.py
@@ -1,18 +1,16 @@
-from problem_two import main
-from services.math_service import MathService
 import pytest
+from problem_two import main
+
 
 @pytest.fixture
-def math_service_mock():
+def mock_math_service():
     class MathServiceMock:
         def fibonacci_sum(self, limit):
             return 42
 
     return MathServiceMock()
 
-def test_main_with_mock(math_service_mock, capsys):
-    main(math_service_mock)
+def test_main_with_mock(mock_math_service, capsys):
+    main(mock_math_service)
     captured = capsys.readouterr()
     assert "The sum of even-valued terms in the Fibonacci sequence below 4000000 is: 42" in captured.out
-
-# Add more test cases as needed
